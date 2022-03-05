72b9699f45708595eb8bc5e30d79960da2f704d2
Matthew Maggiolini <magg01@gmail.com>
2022-03-05 22:37:01 +0100

    + Recipe book screen.

---
+++ RecipeBookScreen.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import {StyleSheet, SafeAreaView, ScrollView, Text} from 'react-native';
+import CocktailTile from './CocktailTile';
+import { getAllRecipes } from './RecipeBook';
+
+
+export function RecipeBookScreen({navigation, route}){
+  const [recipeBook, setRecipeBook] = useState(undefined);
+
+  useEffect(() => {
+    (async() => {
+      await getAllRecipes()
+      .then((returnedRecipes) => {
+        console.log("returned recipes are " + JSON.stringify(returnedRecipes));
+        setRecipeBook(returnedRecipes);
+      });
+    })();
+  }, []);
+
+  if(recipeBook === undefined){
+    return (
+      <Text>
+        add loading indicator
+      </Text>
+    )
+  } else if (recipeBook === []){
+    return (
+      <Text>
+        hmm... no recipes
+      </Text>
+    )
+  } else if (recipeBook === "error"){
+    return (
+      <Text>
+        There was an error retrieving your recipe book.
+      </Text>
+    )
+  } else {
+    return (
+      <SafeAreaView>
+        <ScrollView contentContainerStyle={{ paddingBottom: 1000 }}>
+          {recipeBook.drinks.map((drink) => (
+            <CocktailTile
+              key={drink["idDrink"]}
+              title={drink["strDrink"]}
+              image={drink["strDrinkThumb"]+"/preview"}
+              onPress={() => {navigation.navigate("CocktailDetail", {drink})}}
+            />
+          ))}
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
