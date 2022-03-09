0d6f29964294e06c438c1b02060c85cc8f04689a
Matthew Maggiolini <magg01@gmail.com>
2022-03-09 15:07:58 +0100

    app wrapped to allow toast messages

--- App.js
+++ App.js
@@ -7,49 +7,52 @@ import { SearchScreen } from './SearchScreen';
 import { RecipeBookScreen } from './RecipeBookScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { RootSiblingParent } from 'react-native-root-siblings';
 
 const Stack = createStackNavigator();
 
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="MainScreen"
-          component={MainScreen}
-          options= {{ title: "Home"}}
-        />
-        <Stack.Screen
-          name="SearchScreen"
-          component={SearchScreen}
-          options= {{ title: "Search results"}}
-        />
-        <Stack.Screen
-          name="CocktailDetailApi"
-          component={CocktailDetailApi}
-          options= {
-            ({route}) => ({
-              title: route.params.drink["strDrink"]
-            })
-          }
-        />
-        <Stack.Screen
-          name="CocktailDetailRecipeBook"
-          component={CocktailDetailRecipeBook}
-          options= {
-            ({route}) => ({
-              title: route.params.drink["strDrink"]
-            })
-          }
-        />
-        <Stack.Screen
-          name="RecipeBookScreen"
-          component={RecipeBookScreen}
-          options={{title: "Recipe book"}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RootSiblingParent>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MainScreen"
+            component={MainScreen}
+            options= {{ title: "Home"}}
+          />
+          <Stack.Screen
+            name="SearchScreen"
+            component={SearchScreen}
+            options= {{ title: "Search results"}}
+          />
+          <Stack.Screen
+            name="CocktailDetailApi"
+            component={CocktailDetailApi}
+            options= {
+              ({route}) => ({
+                title: route.params.drink["strDrink"]
+              })
+            }
+          />
+          <Stack.Screen
+            name="CocktailDetailRecipeBook"
+            component={CocktailDetailRecipeBook}
+            options= {
+              ({route}) => ({
+                title: route.params.drink["strDrink"]
+              })
+            }
+          />
+          <Stack.Screen
+            name="RecipeBookScreen"
+            component={RecipeBookScreen}
+            options={{title: "Recipe book"}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RootSiblingParent>
   );
 }
 
