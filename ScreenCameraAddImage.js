6b9e5c84840ba714d81a5bae52266492e2ff7d6c
Matthew Maggiolini <magg01@gmail.com>
2022-03-10 22:52:23 +0100

    camera ratio set to 1:1 or best match for Android. Views refactored

--- ScreenCameraAddImage.js
+++ ScreenCameraAddImage.js
@@ -4,13 +4,14 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import { Ionicons } from '@expo/vector-icons';
 import { replaceImageForDrink, copyImageFromCache } from './RecipeBook';
+import { LoadingAnimation } from './LoadingAnimation';
 
 export function ScreenCameraAddImage({navigation, route}) {
-  const [hasPermission, setHasPermission ] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
   const [cameraReady, setCameraReady] = useState(false);
+  const [cameraRatio, setCameraRatio] = useState(undefined);
   const cameraRef = useRef(undefined);
-  const newImageCacheUri = useRef(undefined);
 
   //get permission just once
   useEffect(() => {
@@ -20,57 +21,62 @@ export function ScreenCameraAddImage({navigation, route}) {
       setHasPermission(status === 'granted');
     })();
   }, []);
+
+  useEffect(async() => {
+    if(cameraRatio === undefined){
+      try{
+        let ratios = await getSupportedRatiosAsync();
+        if(!ratios.includes("1:1")){
+          let bestRatio = null;
+          let bestSelection = ratios[0];
+          for(let i in ratios){
+            const sides = ratios[i].split(":")
+            if(i === 0){
+              bestRatio = 1 - abs(sides[0]/sides[1]);
+              bestSelection = ratios[i];
+            } else if (1 - abs(sides[0]/sides[1]) < bestRatio){
+              bestRatio = 1 - abs(sides[0]/sides[1]);
+              bestSelection = ratios[i];
+            }
+          }
+          setCameraRatio(bestSelection);
+        } else {
+          setCameraRatio("1:1")
+        }
+      } catch (e) {
+        console.log("Error encountered setting cameraRatio -> " + e)
+      }
+    }
+  })
+
   if (hasPermission === null) {
     //this means permission hasn't yet been granted or denied
     // perhaps better here is a loading indicator or a different screen
     return (
-      <View />
+      <LoadingAnimation
+        loadingMessage="Accessing camera"
+      />
     );
   } else if (hasPermission === false) {
-    // permission has been denied. Keep in mind that there are situations where
-    // the user might not even get promted to allow the camera permission, instead
-    // it is automatically denied. For example a phone with child locks on for the
-    // camera. Or a business phone where the company has blocked use of the Camera
-    // The denied permission state is therefore and important one to consider what
-    // to display.
+    // permission has been denied.
     return (
-      <Text>Access to camera is denied</Text>
-      // good to guide user here on how to change the permission in their settings
+      <Text>Access to camera was denied. Please check your permissions for this app in your device's settings and allow access to the camera.</Text>
     );
   } else {
-    // we have permission.
-    // You might notice on android the ratio is incorrect. Need to use
-    // getSupportedRatiosAsync() to find the available ratios and set this
-    // in the Camera component with the 'ratio' property. It gets a little tricky
-    // because you also need to know the screen ratio to pick the best camera
-    // ratio.
     return (
       <View style={styles.container}>
         <Camera
           style={styles.camera}
           type={cameraType}
           //ratio hard coded for my galaxyS20 - not ideal solution
-          ratio="20:9"
+          ratio={cameraRatio}
           onCameraReady={() => setCameraReady(true)}
           ref = {cameraRef}
         >
           <TouchableOpacity
-            style={styles.flipButton}
-            onPress={() => {
-              setCameraType(
-                cameraType === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}
-          >
-            <Ionicons name="camera-reverse-outline" size={28} />
-          </TouchableOpacity>
-          <TouchableOpacity
             style={styles.captureButton}
             onPress={async () => {
               let image = await cameraRef.current.takePictureAsync();
-              // let newImageUri = await replaceImageForDrink(route.params.drinkId, newImageCacheUri)
               let newImageUri = await replaceImageForDrink(route.params.drinkId, image.uri);
               navigation.navigate("CocktailDetailRecipeBook", {newImageUri: newImageUri})
             }}
@@ -90,23 +96,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   camera: {
-    flex: 1,
     width: "100%",
-    height: "100%",
+    aspectRatio: 1,
     alignItems: "center"
-  },
-  flipButton: {
-    backgroundColor: "white",
-    position: "absolute",
-    bottom: "10%",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 20,
   },
   captureButton: {
     backgroundColor: "white",
     position: "absolute",
-    bottom: "25%",
+    bottom: "10%",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 20,
