d29dc86c24660e914a8f0427c4b4def72e6ee739
Matthew Maggiolini <magg01@gmail.com>
2022-03-11 15:27:44 +0100

    basic shopping list screen added

--- ScreenShoppingList.js
+++ ScreenShoppingList.js
@@ -1,15 +1,37 @@
+import React, {useEffect, useState} from 'react';
 import { ScrollView } from 'react-native';
 import { Cell, Section, TableView } from 'react-native-tableview-simple';
+import { getOrCreateShoppingList, setShoppingList } from './ShoppingList';
+import { LoadingAnimation } from './LoadingAnimation';
 
 export function ScreenShoppingList({navigation, route}){
-  return(
-    //change to flat list
-    <ScrollView>
-      <TableView>
-        <Section>
-          <Cell title="Ingredient 1" />
-        </Section>
-      </TableView>
-    </ScrollView>
-  )
+  const [currentShoppingList, setCurrentShoppingList] = useState(undefined);
+
+  useEffect(async()=> {
+    setCurrentShoppingList(await getOrCreateShoppingList());
+  }, [])
+
+  useEffect(() => {
+    console.log(currentShoppingList);
+  }, [currentShoppingList]);
+
+  if(currentShoppingList === undefined){
+    return (
+      <LoadingAnimation
+        loadingMessage="Fetching shopping list"
+      />
+    )
+  } else {
+    return(
+      //change to flat list
+      <ScrollView>
+        <TableView>
+          {currentShoppingList.recipes.map((recipe) => (
+            <Section header={recipe["strDrink"]}>
+            </Section>
+          ))}
+        </TableView>
+      </ScrollView>
+    )
+  }
 }
