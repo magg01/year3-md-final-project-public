b2f24a41be9d6162caf2e60f22f4569a06b57b69
Matthew Maggiolini <magg01@gmail.com>
2022-03-21 00:22:43 +0100

    changes to cocktailDetailApi mirrored in cocktailDetaailrecipeBook

--- CocktailDetailRecipeBook.js
+++ CocktailDetailRecipeBook.js
@@ -1,5 +1,8 @@
 import { useEffect, useState, useCallback } from 'react';
-import {StyleSheet, View, Image, Text, TextInput} from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View, Image, TextInput, Dimensions} from 'react-native';
+import { Appbar, Modal, Portal, Text, Title, Provider } from 'react-native-paper';
+import { TableView, Section, Cell } from 'react-native-tableview-simple';
+import { useFocusEffect } from '@react-navigation/native'
 import {
   getFromRecipeBook,
   updateRecipe,
@@ -7,16 +10,19 @@ import {
   confirmRecipeRemoval,
   addToFavourties,
   removeFromFavourties} from './RecipeBook';
-import { useFocusEffect } from '@react-navigation/native'
 import { addToShoppingList } from './ShoppingList';
 import { LoadingAnimation } from './LoadingAnimation';
 import { CustomNavigationBar } from './CustomNavigationBar';
+import { ingredientListKeysFromApi, measureListKeysFromApi } from './Constants';
 
 export function CocktailDetailRecipeBook({navigation, route}){
   const [notesText, setNotesText] = useState(undefined);
   const [currentDrink, setCurrentDrink] = useState(undefined);
   const [currentImageUri, setCurrentImageUri] = useState(undefined);
   const [isDrinkFavourite, setIsDrinkFavourite] = useState(undefined);
+  const [currentDrinkIngredients, setCurrentDrinkIngredients] = useState(undefined);
+  const [directionsModalVisible, setDirectionsModalVisible] = useState(false);
+  const { width, height } = Dimensions.get('window');
 
   useFocusEffect(
     useCallback(() => {
@@ -37,6 +43,20 @@ export function CocktailDetailRecipeBook({navigation, route}){
     await removeDrink(currentDrink["idDrink"])
     navigation.goBack()
   }
+
+  useEffect(() => {
+    if(currentDrink != undefined){
+      let ingredients = {}
+      for(let key in ingredientListKeysFromApi){
+        if(currentDrink[ingredientListKeysFromApi[key]] && currentDrink[ingredientListKeysFromApi[key]].trim() != ""){
+          ingredients[key] = {ingredient: currentDrink[ingredientListKeysFromApi[key]], measure: currentDrink[measureListKeysFromApi[key]]}
+        } else {
+          break
+        }
+      }
+      setCurrentDrinkIngredients(ingredients);
+    }
+  }, [currentDrink])
 
   useEffect(() => {
     if(currentDrink != undefined){
@@ -64,7 +84,15 @@ export function CocktailDetailRecipeBook({navigation, route}){
     setCurrentDrink(drink);
   }
 
-  if(currentDrink === undefined){
+  const showDirectionsModal = () => {
+    setDirectionsModalVisible(true);
+  }
+
+  const hideDirectionsModal = () => {
+    setDirectionsModalVisible(false);
+  }
+
+  if(currentDrink === undefined || currentDrinkIngredients === undefined){
     return (
       <LoadingAnimation
         loadingMessage="fetching drink"
@@ -73,43 +101,121 @@ export function CocktailDetailRecipeBook({navigation, route}){
     )
   } else {
     return(
-      <View>
-        <Image
-          style={styles.tileImage}
-          source={{uri: route.params.newImageUri ? route.params.newImageUri : currentImageUri}}
-        />
-        <Text>
-          {currentDrink["strAlcoholic"]}
-          {"\n"}
-          Glass: {currentDrink["strGlass"]}
-          {"\n"}
-          Instructions: {currentDrink["strInstructions"]}
-        </Text>
-        <TextInput
-          placeholder="Notes"
-          onChangeText={text => setNotesText(text)}
-          defaultValue={currentDrink["strNotes"]}
-          onBlur={() => saveNotes()}
-        />
-      </View>
+      <SafeAreaView>
+        <ScrollView>
+          <View style={styles.container}>
+            <Image
+              style={styles.cocktailImage}
+              source={{uri: currentDrink["strDrinkThumb"]}}
+              loadingIndicatorSource={require("./assets/cocktail-shaker.png")}
+            />
+            {currentDrink.strImageAttribution ?
+              <Text style={[styles.imageAttributionText, {top: width - 20}]}>Image: {currentDrink.strImageAttribution}</Text>
+              :
+              null
+            }
+            <View style={styles.cocktailDetails}>
+              <TableView>
+                <Section header={"Overview"}>
+                  { currentDrink.strGlass ?
+                      <Cell cellStyle={"RightDetail"} title={currentDrink.strGlass} detail={"glass"} />
+                    :
+                      null
+                  }
+                  { currentDrink.strIBA ?
+                      <Cell cellStyle={"RightDetail"} title={currentDrink.strIBA} detail={"IBA category"} />
+                    :
+                      null
+                  }
+                  { currentDrink.strCategory ?
+                      <Cell cellStyle={"RightDetail"} title={currentDrink.strCategory} detail={"category"} />
+                    :
+                      null
+                  }
+                </Section>
+                <Section header={"Ingredients"}>
+                  {Object.keys(currentDrinkIngredients).map((ingredientKey) => (
+                      <Cell key={ingredientKey} cellStyle={"RightDetail"} title={currentDrinkIngredients[ingredientKey].ingredient} detail={currentDrinkIngredients[ingredientKey].measure} />
+                    :
+                      null
+                  ))}
+                </Section>
+                <Section header={"Directions"}>
+                <Cell
+                  onPress={() => showDirectionsModal()}
+                  contentContainerStyle={{ alignItems: 'flex-start', height: 60 }}
+                  cellContentView={
+                    <Text style={{ flex: 1, fontSize: 16 }}>
+                      {currentDrink["strInstructions"]}
+                    </Text>
+                  }
+                />
+                </Section>
+              </TableView>
+            </View>
+          </View>
+        </ScrollView>
+        <Provider>
+          <Portal>
+            <Modal visible={directionsModalVisible} onDismiss={hideDirectionsModal} contentContainerStyle={styles.modalStyle}>
+              <ScrollView>
+                <Title>{currentDrink.strDrink}</Title>
+                <TableView>
+                  <Section>
+                    {Object.keys(currentDrinkIngredients).map((ingredientKey) => (
+                        <Cell
+                          key={ingredientKey}
+                          cellStyle={"RightDetail"}
+                          title={currentDrinkIngredients[ingredientKey].ingredient}
+                          titleTextStyle={{fontSize: 20}}
+                          detail={currentDrinkIngredients[ingredientKey].measure}
+                          detailTextStyle={{fontSize: 15, color: 'blue'}}
+                        />
+                      :
+                        null
+                    ))}
+                  </Section>
+                  <Text style={styles.modalText}>{currentDrink["strInstructions"]}</Text>
+                </TableView>
+              </ScrollView>
+            </Modal>
+          </Portal>
+        </Provider>
+      </SafeAreaView>
     );
   };
 }
 
 const styles = StyleSheet.create({
-  tile: {
-    height: 200,
-    width: 200,
-    borderRadius: 5,
-    backgroundColor: 'gray',
+  container: {
+    flex: 1,
   },
-  tileImage: {
-    height: 200,
-    width: 200,
-    borderRadius: 5
+  cocktailImage: {
+    width: "100%",
+    aspectRatio: 1,
   },
-  tileTitle: {
+  cocktailDetails: {
+    padding: "2.5%"
+  },
+  imageAttributionText:{
     position: 'absolute',
-    color: 'white'
+    color: 'white',
+    right: 5,
+    fontSize: 10,
+  },
+  directionsText: {
+    fontSize: 20
+  },
+  modalStyle: {
+    backgroundColor: 'white',
+    margin: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  modalText: {
+    fontSize: 20,
+    lineHeight: 50,
   }
 })
